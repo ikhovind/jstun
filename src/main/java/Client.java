78abf3399abe3fb1e99f07eb6f09a4a3a28eacf3
Ingebrigt <ing.hovind@hotmail.com>
2021-03-16 12:46:32 +0100

    started on message header

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,36 +1,34 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
+import java.net.*;
 
 public class Client {
-    private Socket clientSocket;
-    private PrintWriter out;
-    private BufferedReader in;
+    private DatagramSocket socket;
+    private InetAddress address;
 
-    public void startConnection(String ip, int port) throws IOException {
-        clientSocket = new Socket(ip, port);
-        out = new PrintWriter(clientSocket.getOutputStream(), true);
-        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    private byte[] buf;
+
+    public Client() throws SocketException, UnknownHostException {
+        socket = new DatagramSocket();
+        address = InetAddress.getByName("localhost");
     }
 
-    public String sendMessage(String msg) throws IOException {
-        out.println(msg);
-        String resp = in.readLine();
-        return resp;
+    public String sendEcho(String msg) throws IOException {
+        buf = msg.getBytes();
+        DatagramPacket packet
+                = new DatagramPacket(buf, buf.length, address, 4445);
+        socket.send(packet);
+        packet = new DatagramPacket(buf, buf.length);
+        socket.receive(packet);
+        String received = new String(
+                packet.getData(), 0, packet.getLength());
+        return received;
     }
 
-    public void stopConnection() throws IOException {
-        in.close();
-        out.close();
-        clientSocket.close();
+    public void close() {
+        socket.close();
     }
-
     public static void main(String[] args) throws IOException {
         Client client = new Client();
-        client.startConnection("127.0.0.1", 6666);
-        String response = client.sendMessage("hello server");
-        System.out.println(response);
+        client.sendEcho("hei");
     }
 }
--- src/main/java/Stun.java
+++ src/main/java/Stun.java
@@ -2,33 +2,72 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.*;
 
-public class Stun{
+public class Stun extends Thread{
     private ServerSocket serverSocket;
-    private Socket clientSocket;
+    private DatagramSocket clientSocket;
     private PrintWriter out;
     private BufferedReader in;
+    private byte header[] = new byte[20];
+    private int error = 0x0110;
+    private int errorClass =    0b00000100010000;
+    private int bindingMethod = 0b00000000000001;
+    private int successClass =  0b00000100000000;
+    private DatagramSocket socket;
+    private boolean running;
+    private byte[] buf = new byte[256];
+    private int magicCookie = 0x2112A442;
 
-    public void start(int port) throws IOException {
-        serverSocket = new ServerSocket(port);
-        clientSocket = serverSocket.accept();
-        out = new PrintWriter(clientSocket.getOutputStream(), true);
-        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        System.out.println(clientSocket.getInetAddress().toString());
-        System.out.println(clientSocket.getRemoteSocketAddress());
-        out.println("ip: " +  clientSocket.getRemoteSocketAddress());
+    public Stun() throws SocketException {
+        socket = new DatagramSocket(4445);
     }
 
-    public void stop() throws IOException {
-        in.close();
-        out.close();
-        clientSocket.close();
-        serverSocket.close();
+    //TODO is temorarily a string
+    //TODO message length
+    public String formulateHeader(boolean success){
+
+        //legger på 0 helt til lengden er delelig på 16
+        String response = String.format("%16s", Integer.toBinaryString(
+                (success ? successClass : errorClass) | bindingMethod
+                            )).replace(' ', '0');
+        response += String.format("%16s", Integer.toBinaryString(magicCookie));
+        return response;
     }
+    public void run() {
+        running = true;
+
+        while (running) {
+            DatagramPacket packet
+                    = new DatagramPacket(buf, buf.length);
+            try {
+                socket.receive(packet);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            InetAddress address = packet.getAddress();
+            int port = packet.getPort();
+            packet = new DatagramPacket(buf, buf.length, address, port);
+            String received = new String(packet.getData(), 0, packet.getLength());
+
+            System.out.println(packet.getSocketAddress());
+            System.out.println(formulateHeader(true).length());
+            if (received.equals("end")) {
+                running = false;
+                continue;
+            }
+            try {
+                socket.send(packet);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        socket.close();
+    }
+
     public static void main(String[] args) throws IOException {
         Stun server=new Stun();
-        server.start(6666);
+        server.start();
     }
 }
