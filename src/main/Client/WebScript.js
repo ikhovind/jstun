bc8d63d9a2ecd05f5673a416597a3217ee1ef8b3
Ingebrigt <ing.hovind@hotmail.com>
2021-03-20 13:14:46 +0100

    chatchannel now opens using gui

--- src/main/Client/WebScript.js
+++ src/main/Client/WebScript.js
@@ -1,79 +1,68 @@
+window.onload = () => {
+    document.getElementById("leftInputBox").readOnly = true;
+
+    if(localStorage.getItem("joinGenerate") === "join"){
+        document.getElementById("chatButton").addEventListener("click", ()=> acceptOffer());
+    }
+    else{
+        createOffer();
+        document.getElementById("chatButton").addEventListener("click", ()=> openConnecion());
+      }
+}
+
+let chatChannel;
+
+const WebRTCConnection = new RTCPeerConnection({
+    iceServers: [
+        {
+            urls: 'stun:13.48.195.80:3478',
+        },
+    ],
+});
 
 function createOffer(){
-    const WebRTCConnection = new RTCPeerConnection({
-        iceServers: [
-            {
-                urls: 'stun:13.48.195.80:3478',
-            },
-        ],
-    });
-
-    const chatChannel = WebRTCConnection.createDataChannel('chat');
+    chatChannel = WebRTCConnection.createDataChannel('chat');
     chatChannel.onmessage = (event) => console.log('onmessage:', event.data);
-    chatChannel.onopen = () => {
-        console.log('onopen');
-        sessionStorage.setItem("someVarKey", WebRTCConnection);
-        window.location.href = "chatGUI.html";
-    }
+    chatChannel.onopen = () => console.log('onopen');
     chatChannel.onclose = () => console.log('onclose');
 
     WebRTCConnection.onicecandidate = (event) => {
         if (event.candidate)
-            console.log('localDescription:', JSON.stringify(WebRTCConnection.localDescription));
+            document.getElementById("leftInputBox").value = JSON.stringify(WebRTCConnection.localDescription);
+        document.getElementById("leftInputBox").readOnly = true;
     };
 
     WebRTCConnection.createOffer().then((localDescription) => {
         WebRTCConnection.setLocalDescription(localDescription);
-        document.getElementById("descriptor1").value = localDescription;
     });
-
-    return false;
 }
 
 function acceptOffer(){
-    const remoteDescription = document.getElementById("descriptor1").value;
+    const remoteDescription = document.getElementById("rightInputBox").value;
 
-    const WebRTCConnection = new RTCPeerConnection({
-        iceServers: [
-            {
-                urls: 'stun:13.48.195.80:3478',
-            },
-        ],
-    });
 
-    let chatChannel;
     WebRTCConnection.ondatachannel = (event) => {
         if (event.channel.label == 'chat') {
             chatChannel = event.channel;
             chatChannel.onmessage = (event) => console.log('onmessage:', event.data);
-            chatChannel.onopen = () => {
-                console.log('onopen');
-                sessionStorage.setItem("someVarKey", WebRTCConnection);
-                window.location.href = "chatGUI.html";
-            }
+            chatChannel.onopen = () => console.log('onopen');
             chatChannel.onclose = () => console.log('onclose');
-            sessionStorage.setItem("Connection", WebRTCConnection)
         }
     };
 
     WebRTCConnection.onicecandidate = (event) => {
         if (event.candidate)
-            console.log('localDescription:', JSON.stringify(WebRTCConnection.localDescription));
+            document.getElementById("leftInputBox").value = JSON.stringify(WebRTCConnection.localDescription);
     };
 
-    WebRTCConnection.setRemoteDescription(remoteDescription);
+    WebRTCConnection.setRemoteDescription(JSON.parse(remoteDescription));
 
     WebRTCConnection.createAnswer().then((localDescription) => {
         WebRTCConnection.setLocalDescription(localDescription);
-        document.getElementById("descriptor2").value = localDescription;
     });
-
-    window.sessionStorage.setItem("someVarKey", WebRTCConnection);
-
-    return false;
 }
 
 function openConnecion(){
-    const remoteDescription = document.getElementById("descriptor2").value;
-    WebRTCConnection.setRemoteDescription(remoteDescription);
+    const remoteDescription = document.getElementById("rightInputBox").value;
+    WebRTCConnection.setRemoteDescription(JSON.parse(remoteDescription));
 }
