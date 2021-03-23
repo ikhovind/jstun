7f385406807eb99cdbaa1408f6c5f78b035a4c5a
Ingebrigt <ing.hovind@hotmail.com>
2021-03-23 19:38:33 +0100

    log in response, & delete target

--- src/main/java/Response.java
+++ src/main/java/Response.java
@@ -1,7 +1,10 @@
+import org.slf4j.LoggerFactory;
+
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 public class Response {
+    private org.slf4j.Logger log = LoggerFactory.getLogger(Stun.class);
     private static final int magicCookie = 0x2112A442;
     private static final int errorClass = 0b00000100010000;
     private static final int bindingMethod = 0b00000000000001;
@@ -9,12 +12,15 @@ public class Response {
     private byte[] responseBytes;
 
     public Response(boolean validMessage, byte[] data){
+        log.info("formulating header");
         formulateHeader(validMessage, data);
     }
     //with byte for data and ip then this can be used for tcp-connections as well
     public void insertMappedAddress(byte ip[], int port){
+        log.info("inserting mapped address");
         boolean ipv6 = false;
         if (ip.length > 4) {
+            log.info("using ipv6");
             ipv6 = true;
         }
         byte[] attribute = new byte[((64 + (ipv6 ? 128 : 32)) / 8)];
@@ -35,18 +41,16 @@ public class Response {
     }
 
     public void insertXorMappedAdress(byte[] ip, byte[] data, int port) throws IllegalArgumentException {
+        log.info("inserting xor-mapped address");
         byte[] transactionID = getTransactionID(data);
-        Boolean ipv6 = false;
+        boolean ipv6 = false;
         int length = 0x0008;
-        int family = 0x01;
-        System.out.println(ip.length + " for ip " + Arrays.toString(ip));
         if (ip.length == 16) {
+            log.info("using ipv6");
             length = 0x0014;
-            family = 0x02;
             ipv6 = true;
-            System.out.println("IPv6 address found");
         } else if (ip.length > 16) {
-            System.out.println("what in tarnation is this: " + Arrays.toString(ip));
+            log.error("Found invalig ip: " + new String(ip, StandardCharsets.UTF_8));
             throw new IllegalArgumentException("illegal ip-address");
         }
         byte[] attribute = new byte[32/8 + length];
@@ -59,7 +63,7 @@ public class Response {
         attribute[5] = (byte) (ipv6 ? 0x2 : 0x1);
 
         int binCookie = magicCookie >> 16;
-        int binPort = port;
+
         int xport = port ^ binCookie;
         //x-port
         attribute[6] = (byte) (xport >> 8);
@@ -90,15 +94,20 @@ public class Response {
     }
 
     public void insertErrorCodeAttribute(int code, String errorMessage){
+        log.info("inserting error code: " + code + " with error message " + errorMessage);
         //smallest possible value, will be expanded probably
         byte[] attribute = new byte[8];
         //attribute type
         attribute[1] = 9;
         if(errorMessage == null) throw new IllegalArgumentException("error message can't be null");
         if(errorMessage.length() > 127 || errorMessage.getBytes(StandardCharsets.UTF_8).length > 763){
+            log.error("error message too long");
             throw new IllegalArgumentException("Given error message exceeds maximum length of 127 characters or 763 bytes");
         }
-        if(!(code > 299 && code < 700)) throw new IllegalArgumentException("Given code is not an error code");
+        if(!(code > 299 && code < 700)) {
+            log.error("code: " + code + " is not a valid error code");
+            throw new IllegalArgumentException("Given code is not an error code");
+        }
 
         //class
         attribute[6] = (byte) (code/100);
@@ -107,7 +116,6 @@ public class Response {
         //reason phrase
         byte[] reasonBytes = errorMessage.getBytes(StandardCharsets.UTF_8);
         //expand array
-        //todo this pads after, maybe you're supposed to pad before
         int lengthBeforePadding = reasonBytes.length + attribute.length;
 
 
@@ -125,8 +133,8 @@ public class Response {
     }
 
     public void insertUnknownAttributes(Integer[] unknownAttributes){
+        log.info("inserting unknown attributes: " + Arrays.toString(unknownAttributes));
         //pads length up to nearest 4th byte no padding if already divisible
-        //todo pads after, maybe pad before
         int attributeLength = (4 + unknownAttributes.length * 2 + 3) / 4 * 4;
                 //(3 - (unknownAttributes.length * 2 + 3) % 4);
         byte[] attribute = new byte[attributeLength];
@@ -149,8 +157,8 @@ public class Response {
         addToResponse(attribute);
     }
 
-    private static byte[] getTransactionID(byte[] data) {
-
+    private byte[] getTransactionID(byte[] data) {
+        log.info("getting transaction id");
         byte[] transactionID = new byte[12];
 
         for (int i = 0; i < 12; i++) {
@@ -160,13 +168,15 @@ public class Response {
     }
 
     private void addToResponse(byte[] toBeAdded){
+        log.info("concatenating onto response");
         byte[] newBytes = new byte[responseBytes.length + toBeAdded.length];
-        for(int i = 0; i < responseBytes.length; i++) newBytes[i] = responseBytes[i];
-        for (int i = 0; i < toBeAdded.length; i++) newBytes[responseBytes.length + i] = toBeAdded[i];
+        System.arraycopy(responseBytes, 0, newBytes, 0, responseBytes.length);
+        System.arraycopy(toBeAdded, 0, newBytes, responseBytes.length, toBeAdded.length);
         responseBytes = newBytes;
     }
 
     private void formulateHeader(boolean success, byte[] data) {
+        log.info("formulating inital header with " + (success ? "success" : "failure"));
         byte[] headerBytes = new byte[20];
         int messageType = (success ? successClass : errorClass) | bindingMethod;
 
@@ -186,15 +196,15 @@ public class Response {
 
         responseBytes = headerBytes;
     }
-
     private void insertLength(){
+        log.info("inserting length into header");
         int byteArrayLength = responseBytes.length - 20;
         responseBytes[2] = (byte) (byteArrayLength >> 8);
         responseBytes[3] = (byte) (byteArrayLength & 0xff);
-        //  Overwrites the header's placeholder for length.
     }
 
     public byte[] getByteResponse(){
+        log.info("getting bytes of response");
         insertLength();
         return responseBytes;
     }
